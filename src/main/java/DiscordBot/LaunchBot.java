b9c8a551085b4ceb52d6b9c0c6fd10f18360cca8
DigitalRedPanda <osama-12341905@hotmail.com>
2022-02-08 16:09:41 -0800

    Updating

--- src/main/java/DiscordBot/LaunchBot.java
+++ src/main/java/DiscordBot/LaunchBot.java
@@ -1,7 +1,11 @@
 package DiscordBot;
 
+import discord4j.core.DiscordClient;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.rest.request.RouterOptions;
+
 public class LaunchBot {
     public static void main(String[] args){
-
+        DiscordClientBuilder<DiscordClient, RouterOptions> client = DiscordClientBuilder.create("TOKEN");
     }
 }
