183e0fdfba46c1dffd99bf3e79124871bfffa3e5
DigitalRedPanda <osama-12341905@hotmail.com>
2022-02-09 16:34:08 -0800

    Updating

--- src/main/java/DiscordBot/LaunchBot.java
+++ src/main/java/DiscordBot/LaunchBot.java
@@ -10,12 +10,33 @@ import reactor.core.publisher.Mono;
 import java.util.Objects;
 import static DiscordBot.Resources.Token;
 public class LaunchBot {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
 
         DiscordClient client = DiscordClient.create(Objects.requireNonNull(Token));
-        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> gateway.on(ReadyEvent.class, event -> Mono.fromRunnable(() -> {
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
+                gateway.on(ReadyEvent.class, event ->
+                                Mono.fromRunnable(() -> {
             final User self = event.getSelf();
             System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
-        })));
+        })
+
+        ));
         login.block();
-    }}
+        Mono<Void> message = client.withGateway((GatewayDiscordClient gateway) ->{
+        gateway.on(MessageCreateEvent.class, messageCreateEvent -> {
+            Message SentMessage = messageCreateEvent.getMessage();
+            User user = messageCreateEvent.getMember().get();
+            System.out.printf("[%s] %s: %s",SentMessage.getChannel(), user, SentMessage.getContent());
+            if (SentMessage.getContent().equalsIgnoreCase("!ping")) {
+                return SentMessage.getChannel()
+                    .flatMap(channel -> channel.createMessage("pong!"));
+            }
+            return Mono.empty();
+        });
+
+            return Mono.empty();
+        });
+    message.block();
+    }
+
+}
