51c6f743ae334376d015c5c4b4588a4c68e0af31
DigitalRedPanda <osama-12341905@hotmail.com>
2022-02-10 13:05:10 -0800

    Updating

--- src/main/java/DiscordBot/LaunchBot.java
+++ src/main/java/DiscordBot/LaunchBot.java
@@ -6,37 +6,39 @@ import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import java.util.Objects;
+
 import static DiscordBot.Resources.Token;
 public class LaunchBot {
     public static void main(String[] args){
 
         DiscordClient client = DiscordClient.create(Objects.requireNonNull(Token));
-        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
-                gateway.on(ReadyEvent.class, event ->
-                                Mono.fromRunnable(() -> {
-            final User self = event.getSelf();
-            System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
-        })
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->{
+           Mono<Void> printreadiness = gateway.on(ReadyEvent.class, readyEvent ->{
+                Mono.fromRunnable(() ->{
+                   final User self = readyEvent.getSelf();
+                   System.out.printf("Logged in as %s#%s%n",self.getUsername(),self.getDiscriminator());
+                       });
+                return Mono.empty();
+                        }).then();
+            Mono<Void> CreateMessage = gateway.on(MessageCreateEvent.class, messageCreateEvent -> {
+                if(messageCreateEvent.getMember().get().isBot())
+                    return Mono.empty();
 
-        ));
+                Message message = messageCreateEvent.getMessage();
+                if(message.getContent().equalsIgnoreCase("Yo")){
+                    return message.getChannel()
+                            .flatMap(channel -> channel.createMessage("yo"));
+                }
+                return Mono.empty();
+            }).then();
+
+            return printreadiness.and(CreateMessage);
+        });
+
         login.block();
-        Mono<Void> message = client.withGateway((GatewayDiscordClient gateway) ->{
-        gateway.on(MessageCreateEvent.class, messageCreateEvent -> {
-            Message SentMessage = messageCreateEvent.getMessage();
-            User user = messageCreateEvent.getMember().get();
-            System.out.printf("[%s] %s: %s",SentMessage.getChannel(), user, SentMessage.getContent());
-            if (SentMessage.getContent().equalsIgnoreCase("!ping")) {
-                return SentMessage.getChannel()
-                    .flatMap(channel -> channel.createMessage("pong!"));
-            }
-            return Mono.empty();
-        });
-
-            return Mono.empty();
-        });
-    message.block();
     }
 
 }
